2e5bec5c55dc12c4dd0459c763c3c94bf8996f5f
Molly Starnes <mstrnes2@gmail.com>
2023-11-01 14:45:21 -0500

    redid file structure and deleted files

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
 
-connect('mongodb://127.0.0.1:27017/videosAndResponses');
+mongoose.connect('mongodb://localhost:27017/socialMediaDB');
 
-module.exports = connection;
+
+module.exports = mongoose.connection;
--- models/user.js
+++ models/user.js
@@ -1,5 +1,27 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: {type: String, require: true}
+    username: {
+        type: String, 
+        require: true
+    },
+    email: {
+        type: String, 
+        require: true,
+        unique: true,
+    },
+    thoughts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Thought',
+        },
+    ],
+    friends: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
 });
+
+module.exports = mongoose.model('User', userSchema);
